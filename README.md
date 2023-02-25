36af0932e01f85e2e19c615aa352ae56f36cb984
a.moghaddam <aliam1991@gmail.com>
2023-02-25 16:41:52 +0330

    add some slash commands and functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-![Alt text](https://raw.githubusercontent.com/alisvila/kin-ranking/main/public/kin.svg "a title")
+![Alt text](https://raw.githubusercontent.com/alisvila/kin-ranking/main/public/kin.svg "kin ranking")
 
 # kin ranking discord bot
+
+simple discord bot for ranking players based on event
--- handler/loadEvents.js
+++ handler/loadEvents.js
@@ -1,8 +1,8 @@
+const Discord = require("discord.js");
 const clientEvent = (event) => require(`../events/client/${event}`);
 const guildEvent = (event) => require(`../events/guild/${event}`);
 const menuEvents = (event) => require(`../events/interactions/menus/${event}`);
 const otherEvent = (event) => require(`../events/functions/${event}`);
-const Discord = require("discord.js");
 
 function loadEvents(client) {
   const cooldowns = new Discord.Collection();
